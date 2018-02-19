1da3db54dcebd6566194a2a89417b525dc5cb581
mfrashad <m.fathyrashad@gmail.com>
2018-02-19 12:27:07 +0800

    updated

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,2 +1,14 @@
-
-console.log('it works?');
+$(document).ready(function(){
+    $('#result').hide();
+    $('#jsonDiv').hide();
+    $('#form').submit(function(event){
+        event.preventDefault();
+        console.log(window.location.href+'api/new/'+$('input[name="url"]').val());
+        $.getJSON(window.location.href+'api/new/'+$('input[name="url"]').val(),function(data){
+            $('#shortUrl').text(data.shortUrl);
+            $('#result').show();
+            $('#jsonText').text(JSON.stringify(data));
+            $('#jsonDiv').show();
+        });
+    })
+});
